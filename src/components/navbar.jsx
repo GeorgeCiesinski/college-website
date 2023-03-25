9f34276136ab4f961930437d5ed794fde8f36db6
George Ciesinski <george.ciesin@gmail.com>
2023-03-24 20:52:51 -0400

    Hamburger Menu - Added mdi/react - Added mdi/js - Added react-icons - Created hamburger menu icon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import { useState } from 'react';
-import logo from '../assets/images/logo.png';
 import { navLinks } from '../data/constants';
+import logo from '../assets/images/logo.png';
+import { mdiMenu, mdiClose } from '@mdi/js';
+import { Icon } from '@mdi/react';
 
-const navbar = () => (
-    <nav className='w-full flex py-6 justify-between items-center navbar'>
-        <img src={logo} alt='Humber' className='w-[124px]' />
+const navbar = () => {
+    
+    const [toggle, setToggle] = useState(false);
 
-        <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
-            {navLinks.map((nav, index) => (
-                <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
-                    <a href={`#${nav.id}`}>
-                        {nav.title}
-                    </a>
-                </li>
-            ))}
-        </ul>
+    return (
+        <nav className='w-full flex py-6 justify-between items-center navbar'>
+            <img src={logo} alt='Humber' className='w-[124px]' />
 
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
-            
-        </div>
-    </nav>
-)
+            <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+                {navLinks.map((nav, index) => (
+                    <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}>
+                        <a href={`#${nav.id}`}>
+                            {nav.title}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className='sm:hidden flex flex-1 justify-end items-center'>
+                <Icon 
+                    path={toggle ? mdiClose : mdiMenu} 
+                    size={1} 
+                    alt='menu'
+                    className='w-[28px] h-[28px]'
+                    onClick={() => setToggle((prev) => !prev)}
+                />
+            </div>
+        </nav>
+    )
+}
 
 
 export default navbar
