14030fdcf09edcaf99d9a4b1efcc0500365fca86
George Ciesinski <george.ciesin@gmail.com>
2023-03-28 09:39:25 -0400

    Created Accessibility Layout

--- src/components/accessibility.jsx
+++ src/components/accessibility.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
-import { mdiHuman, mdiClose } from '@mdi/js';
+import { mdiHuman, mdiClose, mdiContrastCircle, mdiFormatSize } from '@mdi/js';
 import { Icon } from '@mdi/react';
 
 const accessibility = () => {
@@ -8,15 +8,136 @@ const accessibility = () => {
 
     return (
         <div>
-            <div className={`${toggle ? 'flex' : 'hidden'} fixed top-0 left-0 w-full sm:w-1/3 h-full z-40 bg-primary1`}>
-                <div className='flex justify-end text-white'>
-                    <Icon 
-                        path={mdiClose} 
-                        size={2} 
-                        alt='accessibility'
+            {/* ${toggle ? 'flex' : 'hidden'} */}
+            <div className={`flex flex-col fixed top-0 left-0 w-full sm:w-1/3 h-full z-40 bg-primary1`}>
+                <div className='w-full flex justify-end text-white'>
+                    <div 
+                        className='w-max h-max hover:bg-white/20 p-2 m-4 rounded-lg'
+                        alt='close-accessibility'
+                        title='Close'
                         onClick={() => setToggle((prev) => !prev)}
-                    />
+                    >
+                        <Icon 
+                            path={mdiClose} 
+                            size={2} 
+                        />
+                    </div>
                 </div>
+                <div className='flex flex-col'>
+                    <div className='flex justify-around text-white'>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='contrast'
+                            title='Contrast'
+                        >
+                            <Icon 
+                                path={mdiContrastCircle} 
+                                size={3} 
+                            />
+                            <p>
+                                Contrast
+                            </p>
+                        </div>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='font-size'
+                            title='Font Size'
+                        >
+                            <Icon 
+                                path={mdiFormatSize} 
+                                size={3} 
+                            />
+                            <p>
+                                Font Size
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex justify-around text-white'>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='contrast'
+                            title='Contrast'
+                        >
+                            <Icon 
+                                path={mdiContrastCircle} 
+                                size={3} 
+                            />
+                            <p>
+                                Contrast
+                            </p>
+                        </div>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='font-size'
+                            title='Font Size'
+                        >
+                            <Icon 
+                                path={mdiFormatSize} 
+                                size={3} 
+                            />
+                            <p>
+                                Font Size
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex justify-around text-white'>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='contrast'
+                            title='Contrast'
+                        >
+                            <Icon 
+                                path={mdiContrastCircle} 
+                                size={3} 
+                            />
+                            <p>
+                                Contrast
+                            </p>
+                        </div>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='font-size'
+                            title='Font Size'
+                        >
+                            <Icon 
+                                path={mdiFormatSize} 
+                                size={3} 
+                            />
+                            <p>
+                                Font Size
+                            </p>
+                        </div>
+                    </div>
+                    <div className='flex justify-around text-white'>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='contrast'
+                            title='Contrast'
+                        >
+                            <Icon 
+                                path={mdiContrastCircle} 
+                                size={3} 
+                            />
+                            <p>
+                                Contrast
+                            </p>
+                        </div>
+                        <div 
+                            className='w-[112px] h-[112px] flex flex-col justify-center items-center hover:bg-white/20 cursor-pointer p-2 m-4 rounded-lg'
+                            alt='font-size'
+                            title='Font Size'
+                        >
+                            <Icon 
+                                path={mdiFormatSize} 
+                                size={3} 
+                            />
+                            <p>
+                                Font Size
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
             <button
                 title='Accessibility'
